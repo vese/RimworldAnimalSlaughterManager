b7f27049b28cc71ad84abf44b8b713fe4e3e95a8
Ilya Veselov <ves.ilya@mail.ru>
2026-08-08 23:55:14 +0300

    Add copy to game folder script

---
+++ CopyToGame.bat
@@ -0,0 +1,30 @@
+@echo off
+
+set "ENV_FILE=%~dp0.env"
+
+:: Чтение всех переменных из .env
+for /f "usebackq delims=" %%i in (`type "%ENV_FILE%" 2^>nul`) do set "%%i"
+
+if not defined DEST (
+    echo ERROR: DEST variable not found in %ENV_FILE%
+    pause
+    exit /b 1
+)
+
+git pull
+
+rd /s /q "%DEST%"
+set "SOURCE=%~dp0About"
+xcopy "%SOURCE%" "%DEST%\About\" /e /i /h /k /y /r
+set "SOURCE=%~dp0Assemblies"
+xcopy "%SOURCE%" "%DEST%\Assemblies\" /e /i /h /k /y /r
+set "SOURCE=%~dp0Defs"
+xcopy "%SOURCE%" "%DEST%\Defs\" /e /i /h /k /y /r
+set "SOURCE=%~dp0Languages"
+xcopy "%SOURCE%" "%DEST%\Languages\" /e /i /h /k /y /r
+set "SOURCE=%~dp0Textures"
+xcopy "%SOURCE%" "%DEST%\Textures\" /e /i /h /k /y /r
+
+start steam://rungameid/294100
+
+:: pause
